74212b7f28c1cbe86a6b7a466649c6972bb22d34
saurabh sharma <81613615+saurabh7310@users.noreply.github.com>
2023-02-18 21:47:36 +0530

    Data Transformation completed

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Insurance.entity import config_entity
 import os, sys
 from Insurance.components.data_ingestion import DataIngestion
 from Insurance.components.data_validation import DataValidation
+from Insurance.components.data_transformation import DataTransformation
+
 
 # def test_logger_and_exception():
 #     try:
@@ -32,9 +34,16 @@ if __name__ == "__main__":
         # Data Validation
         data_validation_config= config_entity.DataValidationConfig(training_pipeline_config= training_pipeline_config)
         data_validation = DataValidation(data_validation_config= data_validation_config,
-                                         data_ingestion_artifact= data_ingestion_config)
+                                         data_ingestion_artifact= data_ingestion_artifact)
         
         data_validation_artifact = data_validation.initiate_data_validation()
 
+        # Data Transformation 
+        data_transformation_config= config_entity.DataTransformationConfig(training_pipeline_config= training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config= data_transformation_config,
+                                                 data_ingestion_artifact= data_ingestion_artifact)
+        
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        
     except Exception as e:
         print(e)
